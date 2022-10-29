597e8827d47ec155bff736698595acf866a6b46a
Aaron McCament <amccamen@uccs.edu>
2022-10-29 01:03:52 +0000

    first specs created

--- spec/example/first_spec.rb
+++ spec/example/first_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "The math below is wrong..." do
   it "should equal 42 but we said 43" do
-    expect(6 * 7).to eq(43)
+    expect(6 * 7).to eq(42)
   end
 end
 
---
+++ spec/example/second_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "hello spec" do
+  # ...
+  describe String do
+    let(:string) { String.new }
+    it "should provide an empty string" do
+      expect(string).to eq("")
+    end
+  end
+ end
+
