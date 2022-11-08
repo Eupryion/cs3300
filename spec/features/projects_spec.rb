5b0b86fabdbb0545c2eebaca7d9923e7f62bcc09
Aaron McCament <amccamen@uccs.edu>
2022-11-08 12:25:17 +0000

    get test to work

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -49,7 +49,7 @@ RSpec.feature "Projects", type: :feature do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
     scenario "remove project" do
       visit projects_path
-      click_link "Destroy"
+      click_button "Destroy this project"
       expect(page).to have_content("Project was successfully destroyed")
       expect(Project.count).to eq(0)
     end
