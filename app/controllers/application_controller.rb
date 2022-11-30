61b3bc2f0ceb3f075ded1c7ef96f258fc2f8aa76
Aaron McCament <amccamen@uccs.edu>
2022-11-30 13:21:11 +0000

    Playing around more with Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def redirect_if_not_signed_in
+    redirect_to root_path if !user_signed_in?
+  end
+  
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
+  
 end
