12f7351f7efe29f6eb7fa75545559c3080c54a11
Aaron McCament <amccamen@uccs.edu>
2022-11-30 16:07:49 +0000

    FINAL VERSION

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,26 +1,15 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[destory] #%i[create edit update destory] #added per iteration 5
+  before_action :authenticate_user!, only: %i[new edit update destory] #added per iteration 5
 
   # GET /projects or /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1 or /projects/1.json
-  def show
-  end
-
   # GET /projects/new
   def new
     @project = Project.new
-  end
-
-  # GET /projects/1/edit
-  def edit
-  end
-
-  def summary
   end
 
   # POST /projects or /projects.json
